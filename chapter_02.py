19a98f6b48fb0404380778c16768ed5f654a2fa6
ishfaqkhan-dev <ishfaqkhan.dev@gmail.com>
2025-07-24 17:26:01 -0800

    Done

---
+++ chapter_02.py
@@ -0,0 +1,81 @@
+
+# marks = 30
+
+# print(marks)
+
+# name = "Ali"
+
+# print(name)
+
+# x1 = 3
+
+# age = 80
+# Age = 23
+
+# comment shortcut = ctrl + /
+# print = 34
+# print(print)
+
+# =======> datatypes < ===============
+
+# a = 5 
+# b = 24.500003452
+# c = 'Hello World 23423, @ # '
+# d = '5'
+# e = True
+
+# ========= > type casting < ===========
+
+# x = '123.567'
+# print(type(x))
+# print(x)
+
+# y = float(x)
+# print(type(y))
+# print(y)
+
+# marks = 45
+# print("Student Marks = ", marks)
+
+#=========> input < ===========
+
+# name = input("What is your Name ?")
+# print("your name is : ", name)
+
+# ========= > Operators < ============
+
+# 1 : arithmeic operater
+
+# a = 10
+# b = 20 
+
+# print("Addition : ", a + b )
+# print("Subtraction : ", a - b )
+# print("multiplication : ", a * b )
+# print("divsion : ", b / a )
+# print("floor Division : ", b // a )
+# print("Reminder : ", b % a )
+# print("Power : ", a ** b )
+
+# ======== > comparison Operator < ========
+
+# x = 11 
+
+# print( x == 10 )
+# print( x > 10 )
+# print( x < 10 )
+# print( x >= 10 )
+# print( x <= 10 )
+# print( x != 10 )
+
+# ======== > logical operator < ==========
+
+# a = 10 
+# b = 15
+
+# print( a == 10 and b == 15)
+# print( a == 12 or b == 15)
+# print(not a == 10)
+
+
+
